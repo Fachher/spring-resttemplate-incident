467b2b121b4fc47f040283173bbcfffbe4920dbb
Fachher Syed <fachher.syed@kuehne-nagel.com>
2025-11-30 19:10:15 +0100

    initial commit

---
+++ src/main/java/com/example/rest_template_incident/RestTemplateIncidentApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_template_incident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateIncidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateIncidentApplication.class, args);
+	}
+
+}
