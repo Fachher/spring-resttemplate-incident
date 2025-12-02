3c23df76beecd5d3e383f92859a48596a4f5a81d
Fachher Syed <fachher.syed@kuehne-nagel.com>
2025-12-02 15:08:40 +0100

    Fixed test WithoutRestConfigurationTest

--- src/main/java/com/example/rest_template_incident/RestConfiguration.java
+++ src/main/java/com/example/rest_template_incident/RestConfiguration.java
@@ -5,11 +5,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
 
 @RequiredArgsConstructor
 @Profile(Profiles.REST_CONFIGURATION)
@@ -20,12 +17,5 @@ public class RestConfiguration {
     public RestTemplate restTemplate(final RestTemplateBuilder builder) {
         return builder.build();
     }
-
-//    @Bean
-//    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-//        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-//        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
-//        return converter;
-//    }
 
 }
